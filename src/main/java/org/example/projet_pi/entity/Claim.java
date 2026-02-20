de5f9d4a9ea958bf85683d4571878af8ebd54e1d
Tasnim847 <tasnim.ghodbane@esprit.tn>
2026-02-20 19:20:30 +0100

    add partie dto

--- src/main/java/org/example/projet_pi/entity/Claim.java
+++ src/main/java/org/example/projet_pi/entity/Claim.java
@@ -3,10 +3,14 @@ package org.example.projet_pi.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Claim {
     @Id
@@ -36,83 +40,4 @@ public class Claim {
     @OneToOne(mappedBy = "claim", cascade = CascadeType.ALL)
     private RiskClaim riskClaim;
 
-    public RiskClaim getRiskClaim() {
-        return riskClaim;
-    }
-
-    public void setRiskClaim(RiskClaim riskClaim) {
-        this.riskClaim = riskClaim;
-    }
-
-    public Long getClaimId() {
-        return claimId;
-    }
-
-    public void setClaimId(Long claimId) {
-        this.claimId = claimId;
-    }
-
-    public Date getClaimDate() {
-        return claimDate;
-    }
-
-    public void setClaimDate(Date claimDate) {
-        this.claimDate = claimDate;
-    }
-
-    public double getApprovedAmount() {
-        return approvedAmount;
-    }
-
-    public void setApprovedAmount(double approvedAmount) {
-        this.approvedAmount = approvedAmount;
-    }
-
-    public double getClaimedAmount() {
-        return claimedAmount;
-    }
-
-    public void setClaimedAmount(double claimedAmount) {
-        this.claimedAmount = claimedAmount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ClaimStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ClaimStatus status) {
-        this.status = status;
-    }
-
-    public InsuranceContract getContract() {
-        return contract;
-    }
-
-    public void setContract(InsuranceContract contract) {
-        this.contract = contract;
-    }
-
-    public Compensation getCompensation() {
-        return compensation;
-    }
-
-    public void setCompensation(Compensation compensation) {
-        this.compensation = compensation;
-    }
-
-    public List<Document> getDocuments() {
-        return documents;
-    }
-
-    public void setDocuments(List<Document> documents) {
-        this.documents = documents;
-    }
 }
